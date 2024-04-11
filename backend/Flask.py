235c1006ae91c6c8f16bf3fc76bd8fd722a4bc1c
Tarun Arumugam <tarun.arumugam7467@gmail.com>
2024-04-11 12:57:23 -0500

    test2

--- backend/Flask.py
+++ backend/Flask.py
@@ -22,13 +22,13 @@ CORS(app)
 #     # send_static_file will guess the correct MIME type
 #     return app.send_static_file(os.path.join('dist', path))
 
-# @app.route('/', defaults={'path': ''})
-# @app.route('/<path:path>')
-# def catch_all(path):
-#     if path != "" and os.path.exists(app.static_folder + '/' + path):
-#         return send_from_directory(app.static_folder, path)
-#     else:
-#         return send_from_directory(app.static_folder, 'index.html')
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
 
 @app.route('/translate_to_braille', methods=['POST'])
 def translate_to_braille_endpoint():
@@ -183,4 +183,4 @@ def relfect_pdf(input_path, output_path):
             pdf_writer.write(output_file)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
