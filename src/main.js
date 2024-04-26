6fa70db5aad7ea61f64309f67e5fa08f23cec259
Tarun Arumugam <tarun.arumugam7467@gmail.com>
2024-04-26 13:22:47 -0500

    Update Heroku Branch

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './assets/global.css';
-import Notifications from '@kyvg/vue3-notification'
-
-const app = createApp(App);
-app.use(Notifications);
-app.mount('#app');
