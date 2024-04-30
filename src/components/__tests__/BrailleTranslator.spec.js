e5ba1a0a688ced1da49aed59c3ebbe1dccfdd033
WilliamTichenor <wctich@gmail.com>
2024-04-30 17:21:08 -0500

    extra test messing

--- src/components/__tests__/BrailleTranslator.spec.js
+++ src/components/__tests__/BrailleTranslator.spec.js
@@ -1,11 +1,23 @@
 import { describe, it, expect } from 'vitest'
 import { mount } from '@vue/test-utils'
-import BrailleTranslator from '../BrailleTranslator.vue'
+import BrailleTranslator from '../../App.vue'
 
 describe('BrailleTranslator.vue Test', () => {
     it('renders message when component is created', () => {
       // render the component
       const wrapper = mount(BrailleTranslator)
+  
+      // check that the text is rendered
+      expect(wrapper.text()).toMatch('Translate PDF')
+    })
+    it('Test text disappears when button is pressed', async () => {
+      // render the component
+      const wrapper = mount(BrailleTranslator)
+      const textBoxInput = wrapper.find('textarea')
+      await textBoxInput.setValue("testing text!")
+      expect(textBoxInput.text()).toMatch('testing text!')
+      const verifyClick = await wrapper.get("button").trigger("click");
+      expect(wrapper.text()).not.toMatch('testing text!')
   
       // check that the text is rendered
       expect(wrapper.text()).toMatch('Translate PDF')
