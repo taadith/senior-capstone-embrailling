3f79146b0e3afad5bd94d093d37fe4681ff5e33a
WilliamTichenor <wctich@gmail.com>
2024-04-30 20:00:23 -0500

    updated spec

--- src/components/__tests__/BrailleTranslator.spec.js
+++ src/components/__tests__/BrailleTranslator.spec.js
@@ -15,7 +15,6 @@ describe('BrailleTranslator.vue Test', () => {
       const wrapper = mount(BrailleTranslator)
       const textBoxInput = wrapper.find('textarea')
       await textBoxInput.setValue("testing text!")
-      expect(textBoxInput.text()).toMatch('testing text!')
       const verifyClick = await wrapper.get("button").trigger("click");
       expect(wrapper.text()).not.toMatch('testing text!')
   
